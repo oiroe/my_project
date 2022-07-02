b535d691f43409015a6ebaa04dc91d546377e1fe
oiroe <pathiti.b@gmail.com>
2022-07-02 22:01:08 +0700

    meaw

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pboonpro <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/02 20:31:13 by pboonpro          #+#    #+#             */
+/*   Updated: 2022/07/02 22:00:48 by pboonpro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	unsigned char	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == 0)
+		return (0);
+	ft_bzero(ptr, mem * count);
+	return (ptr);
+}
